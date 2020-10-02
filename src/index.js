48429d515edb77b7f6a3a8e6a5434da1071f4eaa
Yaroshik Vitali <v.yaroshik@a1qa.com>
2020-10-02 16:58:03 +0300

    fix: provide task solution

--- src/index.js
+++ src/index.js
@@ -1,12 +1,27 @@
+exports.min = function min(array) {
+    if (array == null || array.length <= 0) {
+        return 0;
+    } else {
+        return Math.min.apply(null, array);
+    }
+};
 
-exports.min = function min (array) {
-  return 0;
-}
+exports.max = function max(array) {
+    if (array == null || array.length <= 0) {
+        return 0;
+    } else {
+        return Math.max.apply(null, array);
+    }
+};
 
-exports.max = function max (array) {
-  return 0;
-}
-
-exports.avg = function avg (array) {
-  return 0;
-}
+exports.avg = function avg(array) {
+    if (array == null || array.length <= 0) {
+        return 0;
+    } else {
+        let sum = 0;
+        array.forEach(element => {
+            sum += element;
+        });
+        return sum / array.length;
+    }
+};
